8c98c7f55afe0617d9fd961f8039ca14c7dba5a9
elizar777 <ahmadbekovelizar390@gmail.com>
2024-03-20 03:08:40 +0600

    second homework

---
+++ apps/secondary/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from apps.secondary.models import Secondary, Backend, Frontend, Desinger, Android, Colleagues, Tags, Comments
+
+# from django.contrib.auth.models import User, Group
+
+# Register your models here.
+admin.site.register(Comments)
+admin.site.register(Tags)
+admin.site.register(Secondary)
+admin.site.register(Backend)
+admin.site.register(Frontend)
+admin.site.register(Desinger)
+admin.site.register(Colleagues)
+admin.site.register(Android)
+
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
